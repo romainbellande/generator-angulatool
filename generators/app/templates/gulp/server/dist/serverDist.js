e91f5aad522b69af6ee304a328fa32e77f62423b
Berelindis <romain.bellande@orange.fr>
2016-09-12 21:55:09 +0200

    add gulp server-dist but on the same port 3000 as server-build

--- generators/app/templates/gulp/server/dist/serverDist.js
+++ generators/app/templates/gulp/server/dist/serverDist.js
@@ -2,9 +2,17 @@
   'use strict';
 
   var gulp = require('gulp');
-  var gls = require('gulp-live-server');
   var runSequence = require('run-sequence');
+
+  var gls = require('gulp-live-server');
   var open = require('gulp-open');
+
+  var usemin = require('gulp-usemin');
+  var minifyHtml = require('gulp-htmlmin');
+  var minifyCss = require('gulp-clean-css');
+  var uglify = require('gulp-uglify');
+  var rev = require('gulp-rev');
+
   var path = require('path');
   var baseDir = '../../../';
 
@@ -20,6 +28,23 @@
       server.start();
     });
 
+    gulp.task('server-dist-copy', function () {
+      return gulp.src(getPath('app/build/server/**/*.js'))
+      .pipe(gulp.dest((getPath('app/dist/server'))));
+    });
+
+    gulp.task('server-dist-min', function () {
+      return gulp.src(getPath('app/build/client/index.html'))
+      .pipe(usemin({
+        css: [minifyCss(), rev()],
+        html: [minifyHtml({collapseWhitespace: true})],
+        js: [uglify({mangle: false}), rev()],
+        inlinejs: [uglify({beautify: true, mangle: true})],
+        inlinecss: [minifyCss()]
+      }))
+      .pipe(gulp.dest(getPath('app/dist/client')));
+    });
+
     gulp.task('server-dist-open', function () {
       gulp.src('.').pipe(open({
         uri: 'http://127.0.0.1:3000'
@@ -27,7 +52,7 @@
     });
 
     gulp.task('server-dist', function () {
-      runSequence('server-dist-live', 'server-dist-open');
+      runSequence(['server-dist-min', 'server-dist-copy'], 'server-dist-live', 'server-dist-open');
     });
   };
 
